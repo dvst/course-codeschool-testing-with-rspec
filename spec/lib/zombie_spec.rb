6b8def0fda4eb7cef02bd940ab66cffb55f57c9a
Javier E. Perez P <javier.perez.puello@gmail.com>
2016-11-18 17:03:15 -0400

    now that the spec works (fails right) we start to write real code

--- spec/lib/zombie_spec.rb
+++ spec/lib/zombie_spec.rb
@@ -4,6 +4,7 @@ describe Zombie do
   # nombre del ejemplo (example)
   it "is named Ash" do
     zombie = Zombie.new
+    # "expectation", not assertions
     zombie.name.should == 'Ash'
   end
 end
